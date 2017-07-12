1e68c3b30068df39b6efd9c8b576122d215a5a08
Alejandro de la Fuente <alex695afr@gmail.com>
2017-07-12 18:18:58 +0200

    Commit

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
   username: String,
-  email:    String,
-  password: String
+  email: String,
+  password: String,
+  pic_name: String,
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+
 });
 
 const User = mongoose.model('User', UserSchema);
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,37 +1,66 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const express = require('express');
+const passport = require('passport');
+const multer = require('multer');
+
+const upload = multer({ dest: './public/uploads/' });
+const User = require('../models/user');
+const router = express.Router();
+const {
+  ensureLoggedIn,
+  ensureLoggedOut
+} = require('connect-ensure-login');
+
+
+
+
+
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+  res.render('authentication/login', {
+    message: req.flash('error')
+  });
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
 }));
+// router.post('/signup', upload.single('photo'), function(req, res){
+//
+//   user = new User({
+//
+//     pic_path: `/uploads/${req.file.filename}`,
+//     pic_name: req.file.originalname
+//   });
+//
+//   pic.save((err) => {
+//       res.redirect('/');
+//   });
+// });
 
 router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+  res.render('authentication/signup', {
+    message: req.flash('error')
+  });
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
+router.post('/signup',upload.single('photo'), ensureLoggedOut(), passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash: true
 }));
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-    res.render('authentication/profile', {
-        user : req.user
-    });
+  res.render('authentication/profile', {
+    user: req.user
+  });
 });
 
-router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
-    req.logout();
-    res.redirect('/');
+router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
