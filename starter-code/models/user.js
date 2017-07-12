78f7d690cba20662f34ec6666e82e68c3eb4a1fd
Daniele <daniele.germani.85@gmail.com>
2017-07-12 18:30:05 +0200

    iteration 1 ok

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -4,8 +4,14 @@ const Schema   = mongoose.Schema;
 const UserSchema = Schema({
   username: String,
   email:    String,
-  password: String
+  password: String,
+  pic_name: String
+},
+  {
+ timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
+
+
 
 const User = mongoose.model('User', UserSchema);
 
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,6 +1,9 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: './public/uploads/' });
+const User = require('../models/user');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
@@ -17,7 +20,7 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', upload.single('photo'), ensureLoggedOut(), passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
@@ -33,5 +36,21 @@ router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
 });
+// Route to upload from project base path
+
+
+// router.post('/upload', upload.single('photo'), function(req, res){
+//
+//     user = new User({
+//     name: req.body.name,
+//     pic_path: `/uploads/${req.file.filename}`,
+//     pic_name: req.file.originalname
+//   });
+//
+//   pic.save((err) => {
+//       res.redirect('/');
+//   });
+// });
+
 
 module.exports = router;
