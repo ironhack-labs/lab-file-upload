17bc8b3914ab99576936bef70fd4ef3a77fc816e
Carlos Dueñas <carlosdss84@gmail.com>
2017-07-12 19:48:26 +0200

    control

--- starter-code/app.js
+++ starter-code/app.js
@@ -99,7 +99,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/bower_components', express.static(path.join(__dirname, 'bower_components/')))
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
 app.use(express.static(path.join(__dirname, 'public')));
 
 const index = require('./routes/index');
