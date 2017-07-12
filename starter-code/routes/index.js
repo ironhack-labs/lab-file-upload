3d5bf1ee8d77edc0963c1507f060ba0c1eff2917
mikelRumayor <mikel.rumayor@gmail.com>
2017-07-12 19:01:09 +0200

    iteration 1 done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,7 +3,9 @@ const router  = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  console.log(req.user.pic_path)
+
+  res.render('index', { title: 'Express - Generated with IronGenerator' , user: req.user});
 });
 
 module.exports = router;
