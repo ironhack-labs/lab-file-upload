27bba563e83638e42d4e869bfa3c9e813ce4401f
Jorge Pedrejon <jgpedrejon@gmail.com>
2017-07-13 09:45:09 +0200

    done

--- starter-code/models/post.js
+++ starter-code/models/post.js
@@ -1,16 +1,15 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-const UserSchema = Schema({
-  username: String,
-  email:    String,
-  password: String,
+const PostSchema = Schema({
+  userId: String,
+  comments:    String,
   pic_path : String,
   pic_name: String
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
-const User = mongoose.model('User', UserSchema);
+const Post = mongoose.model('Post', PostSchema);
 
-module.exports = User;
+module.exports = Post;
