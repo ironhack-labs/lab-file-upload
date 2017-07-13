97d13b4eec1bc7a2420306a8ab7f94615621b609
Daniel Fernando Palma Lopez <palma@Daniels-MacBook-Pro.local>
2017-07-13 09:39:19 +0200

    not working

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -4,8 +4,18 @@ const Schema   = mongoose.Schema;
 const UserSchema = Schema({
   username: String,
   email:    String,
-  password: String
-});
+  password: String,
+  profilePicture: {
+    picturePath: {
+        type: String,
+        default: "https://365randommuppets.files.wordpress.com/2014/09/266-beaker1.jpg?w=1200"
+      },
+      pictureName: {
+          type: String,
+          default: "Profile Picture"
+      }
+  }
+  });
 
 const User = mongoose.model('User', UserSchema);
 
