c4343af319f59fe95c2ada8f01edd4f57085e9b7
Mercedes Egido <meegido@gmail.com>
2017-09-13 22:59:38 +0200

    elements to save image but database not exists

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 var multer = require('multer');
 
 /* GET home page. */
+var upload = multer({ dest: './public/uploads/' });
 
 var pictures = [];
 
@@ -11,16 +12,18 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
 });
 
-// router.post('/upload',upload.single('photo'),function(req, res, next) {
-//   console.log(req.body);
-//   console.log(req.file);
-//   pictures.push({
-//     "imgURL": req.file.filename,
-//     "name": req.body.name
-//   });
-//   res.redirect('/');
-// });
+var upload = multer({ dest: './public/uploads/' });
 
-
+router.post('/upload', upload.single('photo'),function(req, res, next) {
+  // console.log(req.body);
+  // console.log(req.file);
+  pictures.push({
+    "imgURL": req.file.filename,
+    "name": req.body.name
+  })
+  .save((err) => {
+      res.redirect('/');
+  });
+});
 
 module.exports = router;
