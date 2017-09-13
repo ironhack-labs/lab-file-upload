97b6ad8705769ff4cb5c4f1f882e7c6fb8da188b
y4izus <yaiza.garcia.mm@gmail.com>
2017-09-13 20:59:58 +0200

    Iteration 1 | User Profile Pictures DONE

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require('mongoose')
+const Schema   = mongoose.Schema
 
 const UserSchema = Schema({
   username: String,
   email:    String,
-  password: String
-});
+  password: String,
+  picture: {
+    pic_path: String,
+    pic_name: String
+  }
+})
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', UserSchema)
 
-module.exports = User;
+module.exports = User
