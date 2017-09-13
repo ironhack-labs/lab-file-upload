7bc002af3ea65e7d51eb9a641583096e6b9897e3
Mercedes Egido <meegido@gmail.com>
2017-09-13 23:56:45 +0200

    not saving any post

--- starter-code/models/Post.js
+++ starter-code/models/Post.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const PostSchema = Schema({
+  title: String,
   content: String,
-  authorId: String,
+  userId: String,
   comment: {
     content: String,
     authorId: String,
--- starter-code/routes/post.js
+++ starter-code/routes/post.js
@@ -7,5 +7,23 @@ router.get('/posts', ensureLoggedIn(), (req, res) => {
     res.render('show', { message: req.flash('error')});
 });
 
+router.post((req, res, next) => {
+  const postContent = {
+    title: req.body.title,
+    content: req.body.content,
+    userId: req.body.userId
+  }
+
+  const post = new Post(newPostContent)
+
+  post.save((error) => {
+    console.log(post)
+    if(error) {
+      next(error)
+    } else {
+      res.render('/posts')
+    }
+  })
+});
 
 module.exports = router;
