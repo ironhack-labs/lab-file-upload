f9796520fcbb028589c4d4f680bf964f33c7ca7a
Mercedes Egido <meegido@gmail.com>
2017-09-13 23:16:05 +0200

    base for post

---
+++ starter-code/models/Post.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const PostSchema = Schema({
+  content: String,
+  authorId: String,
+  comment: {
+    content: String,
+    authorId: String,
+    {
+      timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+    }
+  }
+  img: {
+    pic_path: String,
+    pic_name: String
+  }
+}, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
