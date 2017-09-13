44be74940971111f9303f1de151e989ef7ca853d
Mercedes Egido <meegido@gmail.com>
2017-09-13 20:09:31 +0200

    uploading images at signup

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router  = express.Router();
+var multer = require('multer');
 
 /* GET home page. */
+
+var pictures = [];
+
+
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
 });
+
+// router.post('/upload',upload.single('photo'),function(req, res, next) {
+//   console.log(req.body);
+//   console.log(req.file);
+//   pictures.push({
+//     "imgURL": req.file.filename,
+//     "name": req.body.name
+//   });
+//   res.redirect('/');
+// });
+
+
 
 module.exports = router;
