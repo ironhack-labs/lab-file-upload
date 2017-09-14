baa38f9a50eee388fbe6b63317fae51bad80c70e
Luisa Fernanda Salazar Hoyos <luisafernandasalazarhoyos@MacBook-Pro-de-Luisa.local>
2017-09-14 09:10:38 +0200

    intento fallido de ejercicio

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,34 @@
-const express = require('express');
-const router  = express.Router();
+const express = require('express')
+const router  = express.Router()
+const multer  = require('multer')
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+ router.get('/', (req, res, next) => {
+   res.render('index', { title: 'Express - Generated with IronGenerator' })
 });
+
+// router.get('/', function(req, res, next) {
+//   Picture.find((err, pictures) => {
+//     res.render('index', {pictures})
+//   })
+// });
+
+// const upload = multer({ dest: './public/uploads/' });
+//
+//
+// router.post('/upload', upload.single('photo'), function(req, res){
+//
+//   pic = new Picture({
+//     name: req.body.name,
+//     pic_path: `/uploads/${req.file.filename}`,
+//     pic_name: req.file.originalname
+//   });
+//
+//   pic.save((err) => {
+//       res.redirect('/');
+//   });
+// });
+
+
 
 module.exports = router;
