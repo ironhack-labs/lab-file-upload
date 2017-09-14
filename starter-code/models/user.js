eed2769f278a849e588db400a5b564f96440d618
Danigp <danielentj@gmail.com>
2017-09-14 09:23:29 +0200

    first 1

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
   username: String,
-  email:    String,
-  password: String
+  email: String,
+  password: String,
+  profilePic: String
 });
 
 const User = mongoose.model('User', UserSchema);
