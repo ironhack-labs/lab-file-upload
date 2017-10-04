0adce72905f14f656afd44269d0a2db35153c087
Nicolas <nicolas.barthez@gmail.com>
2017-10-05 00:31:05 +0200

    done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,37 +1,76 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "./public/uploads/" });
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Post = require("../models/post");
 
-router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+router.get("/login", ensureLoggedOut(), (req, res) => {
+  res.render("authentication/login", { message: req.flash("error") });
 });
 
-router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
-}));
+router.post(
+  "/login",
+  ensureLoggedOut(),
+  passport.authenticate("local-login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
 
-router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+router.get("/signup", ensureLoggedOut(), (req, res) => {
+  res.render("authentication/signup", { message: req.flash("error") });
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
-}));
+router.post(
+  "/signup",
+  ensureLoggedOut(),
+  upload.single("photo"),
+  passport.authenticate("local-signup", {
+    successRedirect: "/show",
+    failureRedirect: "/signup",
+    failureFlash: true
+  })
+);
 
-router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-    res.render('authentication/profile', {
-        user : req.user
+router.get("/profile", ensureLoggedIn("/login"), (req, res) => {
+  res.render("authentication/profile", {
+    user: req.user
+  });
+});
+
+router.post(
+  "/show",
+  ensureLoggedIn("/login"),
+  upload.single("picture"),
+  (req, res) => {
+    const newPost = new Post({
+      content: req.body.content,
+      // creatorId: req.user.id,
+      picName: req.body.tag,
+      pic_path: `/uploads/${req.file.filename}`
     });
+    console.log(newPost);
+    newPost.save(err => {
+      if (err) {
+        next(null, false, { message: newPost.errors });
+      }
+    });
+    res.redirect("indexConnected");
+  }
+);
+
+router.post("/logout", ensureLoggedIn("/login"), (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
-router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
-    req.logout();
-    res.redirect('/');
+router.get("/show", ensureLoggedIn("/login"), (req, res) => {
+  res.render("show", {
+    user: req.user
+  });
 });
 
 module.exports = router;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,29 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Post = require("../models/post");
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+router.get("/", ensureLoggedOut(), (req, res, next) => {
+  Post.find({}).exec((err, posts) => {
+    res.render("index");
+  });
+  next();
+});
+
+router.get("/", ensureLoggedIn("/"), (req, res) => {
+  res.render("indexConnected", {
+    user: req.user
+  });
+});
+
+router.get("/indexConnected", ensureLoggedIn("/"), (req, res) => {
+  Post.find({}).exec((err, posts) => {
+    res.render("indexConnected", {
+      user: req.user,
+      posts: posts
+    });
+  });
 });
 
 module.exports = router;
