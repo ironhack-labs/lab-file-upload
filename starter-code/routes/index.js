dc98d546d66e16d9daa11cb7da270b3fdb597a06
Reshma Patel <reshma1529@gmail.com>
2017-10-04 23:18:13 +0200

    Reshma - image not uploaded

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const router  = express.Router();
+const User = require('../models/user');
+var multer  = require('multer');
 
+const router  = express.Router();
+var upload = multer({dest: './public/uploads/'});
+//we require this bcs of we install multer
+
+//var multer  = require('multer');
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
 });
+
+router.post('/uploads',upload.single('photo'), function(req,res){
+  res.send(req.files)
+// const user = new User({
+//   name : req.body.name,
+//   email : req.body.email,
+//   password :req.body.password
+// });
+//   user.save((err) => {
+//     res.redirect('/');
+  // });
+});
+
+router.get('/',function(req, res, next){
+  User.find((err, user))
+})
+
+
 
 module.exports = router;
