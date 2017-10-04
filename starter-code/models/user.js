10fa553b2b350b7bee6dca0e586bd5ea671e5ecd
Thomas <tom.beguin@gmail.com>
2017-10-05 00:31:21 +0200

    draft

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
-  username: String,
-  email:    String,
-  password: String
+	username: String,
+	email: String,
+	password: String,
+	avatar: Object.keys('name', 'path', 'filename'),
+}, {
+	timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
 
 const User = mongoose.model('User', UserSchema);
