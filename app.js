62e739e0504fe40e4c69fac07811951b8263518b
Thibaut Davoult <thibaut.davoult@gmail.com>
2017-10-04 23:54:20 +0200

    done

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require(".env").config();
+require("dotenv").config();
 const express            = require('express');
 const path               = require('path');
 const favicon            = require('serve-favicon');
