9a00d5fb31b0f6d3dd3ff1ef967dae49d68d6251
Elo <wanang.elodie@hotmail.fr>
2017-10-05 18:38:26 +0200

    PAR - Elodie - not done

---
+++ starter-code/routes/posts.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+var multer  = require('multer');
+const Picture = require('../models/pictures');
+const Posts = require('../models/pictures');
+
+
+
+
+router.get('/new'), (res,req,next) {
+
+}
