17ae56813fa35a7e5b5ba8ca7dbbe29baf24a8bd
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-15 19:42:51 +0100

    first commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const multer  = require('multer');
+var upload = multer({ dest: './public/uploads/' });
+
+const Picture = require('../models/picture');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
 });
+
+// Route to upload from project base path
+
+router
+.post('/upload', upload.single('photo'), function(req, res){
+
+  const pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
+});
+
 
 module.exports = router;
