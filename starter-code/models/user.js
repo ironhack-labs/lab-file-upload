73f959b3b56698aa970f1a7021211c567516a950
Alejandro Dietta <alexdietta@hotmail.com>
2017-11-15 23:00:12 +0100

    Iteration 1

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -4,7 +4,11 @@ const Schema   = mongoose.Schema;
 const UserSchema = Schema({
   username: String,
   email:    String,
-  password: String
+  password: String,
+  photo: {
+    pic_path: String,
+    pic_name: String
+  }
 });
 
 const User = mongoose.model('User', UserSchema);
