6be6619c0a97dc91876533688d0de02536699347
gemaderus <hi@gemaderus.com>
2017-11-16 09:13:08 +0100

    Second iteration almost finished.

--- starter-code/app.js
+++ starter-code/app.js
@@ -108,8 +108,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
+const postRoute = require('./routes/posts');
 app.use('/', index);
 app.use('/', authRoutes);
+app.use('/post', postRoute);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
