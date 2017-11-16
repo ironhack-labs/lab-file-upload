03bb6d425bfa029a27f70a3ec9b7a703d1a5c50a
FranRom <maildefranz@gmail.com>
2017-11-16 09:37:30 +0100

    First iteration

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const multer  = require('multer');
+const Picture = require('../models/pictures');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
