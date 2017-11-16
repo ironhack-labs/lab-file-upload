aa73022a6a8e1ef416e196771f849b9457973a53
Asier Cisneros <ascisneros14@gmail.com>
2017-11-16 02:48:43 +0100

    Las primeras 2 iteracciones

---
+++ starter-code/models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const postSchema = new Schema({
+   content: String,
+   pic_path: String,
+   pic_name: String,
+   creatorId: String,
+ }, {
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+});
+
+const Post = mongoose.model('Post', postSchema);
+module.exports = Post;
