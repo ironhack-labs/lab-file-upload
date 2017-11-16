0e00aa24fa68170fee40d36ff077c73fee2bf2cc
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-16 09:25:58 +0100

    view routes and archives

--- starter-code/app.js
+++ starter-code/app.js
@@ -105,9 +105,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
+const postRoutes = require('./routes/posts');
+
 app.use('/', index);
 app.use('/', authRoutes);
-const posts = require('./routes/posts');
+app.use('/posts', postRoutes);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
