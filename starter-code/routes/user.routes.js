76caa00019474fbb8c3ded627498dcb5f11c97c7
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-02-01 12:36:16 +0100

    Fix: Image save

--- starter-code/routes/user.routes.js
+++ starter-code/routes/user.routes.js
@@ -5,7 +5,7 @@ const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 const multer    = require('multer');
 // Route to upload from project base path
-const upload = multer({ dest: '../public/uploads/' });
+const upload = multer({ dest: './public/uploads/' });
 
 router.get('/profile', ensureLoggedIn('/login'),userController.show); 
 router.post('/upload', ensureLoggedIn('/login'),upload.single('photo'),userController.upload); 
