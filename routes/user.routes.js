c9e25e07bd37fd1bd009a0e4ae2b9230d447288d
Mario Velasco Alonso <mario1velasco@gmail.com>
2018-02-02 13:32:23 +0100

    Feat: Update user details

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,7 @@ const secure = require('../configs/passport.config');
 
 router.get('/:id', secure.isAuthenticated, userController.show);
 router.get('/:id/edit', secure.isAuthenticated, userController.update);
+router.post('/:id/', secure.isAuthenticated, userController.doUpdate);
 
 
 // router.post('/signup', secure.nonAuthenticated, userController.doSignup);
