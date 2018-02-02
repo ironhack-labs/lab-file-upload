b7707d3600491752b25a2e501bb1d8b89d5054f3
alltimelaura13 <laura.marcos@ricoh.es>
2018-02-02 09:35:41 +0100

    metodos para upload picture en auth index.js index.ejs proile.ejs

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const router  = express.Router();
-var multer  = require('multer');
-const Picture = require('../models/picture');
+const express = require("express");
+const router = express.Router();
+var multer = require("multer");
+const Picture = require("../models/picture");
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express - Generated with IronGenerator" });
+});
+
+var upload = multer({ dest: "./public/uploads/" });
+
+router.post("/upload", upload.single("photo"), function(req, res) {
+  const pic = new Picture({
+    name: req.body.name,
+    pic_path: `/uploads/${req.file.filename}`,
+    pic_name: req.file.originalname
+  });
+
+  pic.save(err => {
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
