972e88fed053a44354318943a99569a7253b53b9
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-02-07 20:29:16 +0100

    experimento riojano

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router  = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const User = require('../models/user')
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  User
+  // .find({creatorId : User._id})
+  .find(})
+  .sort({
+      created_at: -1
+  })
+  .exec((err, posts) => {
+      console.log(posts);
+      res.render(`/index`, {
+          posts,
+          moment
+      });
+  });
+//ARREGLAR ESTO
+
 });
+
+
 
 module.exports = router;
