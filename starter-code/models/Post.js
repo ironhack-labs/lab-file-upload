9fe74acfcd017051d7c9bf0e7cea6668928ecce5
Susana <susana_vadillo@yahoo.es>
2018-02-08 08:08:28 +0100

    iteracion2

---
+++ starter-code/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+  content: String,
+  creatorId: Schema.Types.ObjectId,
+  picPath: String,
+  picName: String
+});
+
+const Post = mongoose.model("Post", PostSchema);
+module.exports = Post;
