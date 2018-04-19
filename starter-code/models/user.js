036aee07fe5f54609f017c461fe38351e3b1bfec
Mapi Carrillo <mp.carrillo@yahoo.es>
2018-04-19 18:23:19 +0200

    Cambiado usuario

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -4,8 +4,14 @@ const Schema   = mongoose.Schema;
 const UserSchema = Schema({
   username: String,
   email:    String,
-  password: String
+  password: String,
+  picPath: String,
+  picName: String
+},
+{
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
+
 
 const User = mongoose.model('User', UserSchema);
 
