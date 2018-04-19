1cb3f87e7088a341e95daab31df19244d6492011
A-Tr <alvarotrancon@gmail.com>
2018-04-20 00:24:52 +0200

    crea usuarios y posts y los muestra; falta maquetacion

--- starter-code/routes/posting.js
+++ starter-code/routes/posting.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const passport = require("passport");
 const router = express.Router();
 const uploadCloud = require("../config/cloudinary.js");
 const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
