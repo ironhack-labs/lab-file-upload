a5732bbcc9b15b6ec758dd218aa2c1d76f063d58
Yago Vega <yagofvv@gmail.com>
2018-04-19 23:12:23 +0200

    file upload working

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,7 +1,11 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const multer  = require('multer');
+const upload = multer({ dest: './public/uploads/' });
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const Picture = require('../models/picture');
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
@@ -32,6 +36,20 @@ router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
 router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
+});
+router.get('/upload', ensureLoggedIn('/login'), (req, res) => {
+    res.render('authentication/profile-pic');
+});
+router.post('/upload', upload.single('photo'), function(req, res){
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
