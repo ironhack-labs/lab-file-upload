f3f9e96059f8a018b020cc8d145eaefe87460950
A-Tr <alvarotrancon@gmail.com>
2018-04-19 19:25:37 +0200

    intentando subir fotos

---
+++ starter-code/config/cloudinary.js
@@ -0,0 +1,22 @@
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+var storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'lab-upload',
+  allowedFormats: ['jpg', 'png'],
+  filename: function (req, file, cb) {
+    cb(null, 'my-file-name');
+  }
+});
+
+const uploadCloud = multer({ storage: storage });
+module.exports = uploadCloud;
