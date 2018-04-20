4a0bfd02192cd43cfee5eb6740f48d0a6eb24f6b
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-04-20 09:37:04 +0200

    f

--- starter-code/routes/postRouter.js
+++ starter-code/routes/postRouter.js
@@ -1,39 +1,38 @@
-const express    = require('express');
-const passport   = require('passport');
-const postRouter     = express.Router();
-const multer  = require('multer');
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
-const Post = require("../models/post")
-const User = require('../models/user');
-const uploadHandler = multer({ dest: './public/uploads/' });
+const express = require("express");
+const passport = require("passport");
+const postRouter = express.Router();
+const multer = require("multer");
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Post = require("../models/post");
+const User = require("../models/user");
+const uploadHandler = multer({ dest: "./public/uploads/" });
 
 /* GET home page */
-postRouter.get('/', ensureLoggedIn(),(req, res, next) => {
-    res.render('post/index');
-  });
+postRouter.get("/", ensureLoggedIn(), (req, res, next) => {
+  res.render("post/index");
+});
 
-  // GET index
 postRouter.get("/", (req, res, next) => {
-    Post.find()
-    .then( data => {
-      res.render("post/index", {data})
-    })
-  })
-  
-
-  /* RENDER NEW FORM */
-postRouter.get('/new', (req, res, next) => {
-    res.render("post/new");
+  Post.find().then(data => {
+    res.render("post/index", { data });
   });
+});
+
+postRouter.get("/new", (req, res, next) => {
+  res.render("post/new");
+});
 
 // POST new
-postRouter.post("/new", [ensureLoggedIn(), uploadHandler.single("photo")], (req, res, next) => {
+postRouter.post(
+  "/new",
+  [ensureLoggedIn(), uploadHandler.single("photo")],
+  (req, res, next) => {
     const { content } = req.body;
     const picPath = req.file.path;
     const picName = req.file.filename;
-  
+
     const creatorId = req.user.id;
-  
+
     const newPost = new Post({
       content,
       picPath,
@@ -41,19 +40,17 @@ postRouter.post("/new", [ensureLoggedIn(), uploadHandler.single("photo")], (req,
       creatorId
     });
 
-    newPost.save () 
-    .then (() => {
+    newPost
+      .save()
+      .then(() => {
         res.redirect("/post");
-    })
-    
-    .catch((err) => {
+      })
+
+      .catch(err => {
         console.log(err);
         next(err);
       });
-  
-   
-  })
-    
-
+  }
+);
 
 module.exports = postRouter;
