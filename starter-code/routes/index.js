8ebd8baea7f914657b1b3230680850e707821f86
Diego Leal <diegoleallinux@gmail.com>
2018-05-03 17:31:09 -0400

    finished first iteration of uploading pictures with multer

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,32 @@
-const express = require('express');
-const router  = express.Router();
+const express  = require('express');
+const router   = express.Router();
+const multer   = require('multer');
+const Picture  = require('../models/picture');
+const upload   = multer({ dest: './public/uploads'});
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  Picture.find()
+  .then(pictures => {
+    res.render('index', { pictures: pictures });
+  })
+  .catch(err => {
+    console.log(err);
+    next(err);
+  })
+});
+
+router.post('/upload', upload.single('photo'), function(req, res){
+
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
