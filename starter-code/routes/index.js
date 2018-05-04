e7ccdd4c2765b3648fbf4dd109bba2ae03e0878f
Beto <zonanabeto@gmail.com>
2018-05-03 20:49:51 -0500

    me falto mucho

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const router  = express.Router();
+const multer = require("multer");
+const uploadPost = multer({dest: "./public/postPics/"});
+const Post = require("../models/post")
+
+
+router.get("/new",(req,res,next)=>{
+  res.send("hackberto")
+})
+
+router.get("/show",(req,res,next)=>{
+  res.send("hackberto")
+})
+
+router.get("/create",(req,res,next)=>{
+  res.render("create")
+})
+
+router.post("/create",uploadPost.single("picPath"),(req,res,next)=>{
+  req.body.picPath = ("./public/postPics/" + req.file.filename)
+  Post.create(req.body)
+  .then(r=>res.redirect("/new"))
+  .catch(e=>console.log(e))
+})
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
