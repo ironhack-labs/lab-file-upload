6556a9cd1d20792bd2725f4e5b75025cf672d7a3
Alex <alejandrosg11@gmail.com>
2018-05-03 21:08:51 -0500

    partially done

---
+++ starter-code/models/post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    content: String,
+    creatorId: Schema.Types.ObjectId,
+    picPath: String,
+    picName: String 
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+});
+
+module.exports = mongoose.model("Post", postSchema);
