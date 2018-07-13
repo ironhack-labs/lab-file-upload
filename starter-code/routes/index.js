61151aafb0c0697ef72f91d48853309e1f4cd525
Rodrigo Ortiz <roloortizgarcia@gmail.com>
2018-07-13 16:21:23 +0200

    foto subida

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const router  = express.Router();
 const Post = require('../models/Post');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+
 
 //multer
 const multer = require('multer');
 const upload = multer({dest: './public/assets'});
 
+function isAuthenticated(req,res,next){
+  if(req.isAuthenticated()){
+        // req.user
+    return next()
+  }else {
+    res.redirect("/login")
+  }
+}
 
 const cloudinary = require('cloudinary');
 const uploadCloud = require('../helpers/cloudinary')
@@ -14,11 +24,11 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
 });
 
-router.get('/post',(req,res)=>{
+router.get('/post',ensureLoggedIn('/login'),(req,res)=>{
   res.render('posts')
 })
 
-router.post('/post',upload.single('photo'),(req,res)=>{
+router.post('/post',ensureLoggedIn('/login'),upload.single('photo'),(req,res)=>{
   req.body.photo = '/assets/' + req.file.filename;
   
   Post.create(req.body)
