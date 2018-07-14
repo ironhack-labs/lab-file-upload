a6429b62cc788154ca16d7889d2286d59914c031
Verónica Reina <veroreinah@gmail.com>
2018-07-14 08:46:54 +0200

    Iteration 1 | User Profile Pictures

---
+++ starter-code/passport/serializers.js
@@ -0,0 +1,15 @@
+const passport = require("passport");
+const User = require("../models/User");
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
