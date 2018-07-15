8f7bc592e15afc5bd979bd34dc11f6c8f3a2e6aa
Enrique Pernia <Epernia96@gmail.com>
2018-07-16 01:49:55 +0200

    First commit

---
+++ starter-code/models/post.js
@@ -0,0 +1,16 @@
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const PostSchema = Schema({
+  content: String,
+  creatorId:{ type: Schema.Types.ObjectId, ref: 'User' },
+  picPath: String,
+  picName: String,
+  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+   });
+
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
