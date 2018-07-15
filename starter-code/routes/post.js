47711fa7ed1f678a53e1f9d6713d750a3d535da1
dmarinsa <dmarinsanchez@gmail.com>
2018-07-15 17:39:11 +0200

    almost done

--- starter-code/routes/post.js
+++ starter-code/routes/post.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Picture = require('../models/picture');
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 const {ensureLoggedIn} = require('connect-ensure-login');
 const multer = require('multer');
 const upload = multer({dest: './uploads'});
@@ -40,6 +41,28 @@ router.get('/posts/:id', ensureLoggedIn('/login'), (req,res,next)=>{
       res.render('post/post', post)
     })
     .catch(e=>console.log(e));
+})
+
+router.post('/posts/:id/comment', ensureLoggedIn('/login'), (req,res,next)=>{
+  const postImg = new Picture({
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+  
+  const newComment = new Comment({
+    content: req.body.content,
+    creatorId: req.user,
+    img: postImg
+  });
+
+  postImg.save()
+  .then(()=>{
+    newComment.save()
+      .then(()=>{
+        res.redirect(`/post/${req.params.id}`);
+      })
+  })
+  .catch(e=>console.log(e));
 })
 
 
