9d0bbfcb0a3a312bde4dbf4e368bb1a778bdeece
Sarai Vasquez <svmarinez@hotmail.com>
2018-07-15 20:19:19 +0200

    Sunday Afternoon, Iteration 1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router  = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: "./public/uploads/" });
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
