ed43c5eff47f9018a7b538405214bbe70f68f527
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-20 23:29:30 +0200

    first...

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router  = express.Router();
+const multer  = require('multer');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  res.render('index', { title: 'Tumblr' });
 });
 
+
+const upload = multer({ dest: './public/uploads/' });
+
+router.post('/upload', upload.single('photo'), function(req, res){
+
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
+});
 module.exports = router;
