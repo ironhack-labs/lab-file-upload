64146e24a625c008408e410390669e65e02e9696
pablotaboada <pablo.tabu@hotmail.com>
2018-09-21 00:37:36 +0200

    almost done

---
+++ starter-code/routes/posting.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const multer = require('multer');
+const upload = multer({ dest: './public/uploads/' });
+const Post = require('../models/post');
+
+
+router.get("/new-post", (req,res) => {
+ res.render("new-post")
+})
+
+
+router.post('/new-post', [ensureLoggedIn('/login'), upload.single('photo')], (req,res) => {
+    const {content} = req.body;
+    const picpath = `uploads/${req.file.filename}`;
+    const picname = req.file.originalname;
+    const newPost = new Post({
+        content,
+        creatorId: req.user._id,
+        picpath,
+        picname
+    });
+    newPost.save()
+    .then(() => res.redirect('/profile'));
+
+});
+  module.exports = router;
