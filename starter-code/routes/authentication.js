409b52ae05677dcb5edb708148906b69c0efab40
Elvair <elvair@mountain>
2018-09-20 18:01:37 +0200

    Iteración 1 hecha

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,6 +1,8 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const uploadCloud = require('../config/cloudinary.js');
+
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
@@ -17,7 +19,7 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup',[uploadCloud.single('photo'), ensureLoggedOut()], passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
