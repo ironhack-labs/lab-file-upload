6d5dc0686a2afdc87e1e8a2a55adbdd4583d893b
Mapno <mariorvinas@gmail.com>
2018-09-20 20:52:19 +0200

    shows posts in index

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router  = express.Router();
+const Post = require('../models/post');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   const user = req.user;
-  res.render('index', { title: 'Express - Generated with IronGenerator', user });
+  Post.find()
+  .populate('creatorId')
+  .then(posts => {
+    res.render('index', { title: 'Tumblrr', user, posts })
+  })
+  .catch(e => console.log(e))
 });
 
 module.exports = router;
