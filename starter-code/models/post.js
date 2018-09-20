efe5427b0babd1fb6aa52bdbf380d3dacf331957
cesararmada <cesar.armada@gmail.com>
2018-09-20 21:04:37 +0200

    iteration 1 done

---
+++ starter-code/models/post.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const PostSchema = Schema({
+  content: String,
+  creatorId: String,
+  photo:{
+    path: String,
+    originalname:String
+  }
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
