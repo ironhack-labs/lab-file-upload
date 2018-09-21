1b3c0e9e0560d67129b9c0021d3a0cb5675565cf
mish <mish@MacBook-Pro-de-mish.local>
2018-09-21 09:15:59 -0500

    incompleto

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,7 +1,10 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const multer     = require('multer')
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const uploads    = multer({dest:'public/uploads'})
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
@@ -17,11 +20,18 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', uploads.single('image'), ensureLoggedOut(), passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
-}));
+}),(req, res, next)=>{
+    if(req.file)req.body['photoURL'] = `uploads/${req.file.filename}`
+    user.create(req.body)
+    .then(user=>{
+        res.redirect('/authentication/login')
+      })
+    .catch(e=>next(e))
+});
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
