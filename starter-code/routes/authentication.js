9fdcf986c1c1fef1d6c0fb070591cbe5d5c5e29e
Jhon Scarzo <jhon753951@gmail.com>
2018-09-21 02:17:35 +0200

    Small changes towards it 1

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,31 +1,59 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const {
+    ensureLoggedIn,
+    ensureLoggedOut
+} = require('connect-ensure-login');
+const multer = require('multer');
+const Picture = require('../models/pictures');
+const upload = multer({
+    dest: './public/uploads/'
+});
+
+router
+    .post('/upload', upload.single('photo'), function (req, res) {
+
+        const pic = new Picture({
+            name: req.body.name,
+            path: `/uploads/${req.file.filename}`,
+            originalName: req.file.originalname
+        });
+
+        pic.save((err) => {
+            res.redirect('/');
+        });
+    });
+
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+    res.render('authentication/login', {
+        message: req.flash('error')
+    });
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
 }));
 
 router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+    res.render('authentication/signup', {
+        message: req.flash('error')
+    });
 });
 
 router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
 }));
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
-        user : req.user
+        user: req.user
     });
 });
 
@@ -34,4 +62,11 @@ router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     res.redirect('/');
 });
 
+router.get('/', function (req, res, next) {
+    Picture.find((err, pictures) => {
+        res.render('index', {
+            pictures
+        })
+    })
+});
 module.exports = router;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  res.render('index', {
+    title: 'Express - Generated with IronGenerator'
+  });
 });
 
 module.exports = router;
