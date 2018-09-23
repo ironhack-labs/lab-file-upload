9addd2f419a3bb6df5c402465a8f2277ee3fcee0
agdwm <almudenaguerraswm@gmail.com>
2018-09-23 20:12:44 +0200

    Created the model Comment as a subdocument of the Post

--- starter-code/models/Post.js
+++ starter-code/models/Post.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
+const CommentSchema = Schema({
+	content: { type: String, required: true },
+	authorId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+	imagePath: String,
+	imageName: String,
+	date: { type: Date, default: Date.now }
+});
+
 const PostSchema = Schema({
-	content: String,
-	creatorId: { type: Schema.Types.ObjectId, ref: 'User' },
-	picPath: String,
-	picName: String
+	content: { type: String, required: true },
+	creatorId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+	picPath: { type: String, required: true },
+	picName: { type: String, required: true },
+	comments: [CommentSchema]
 }, {
 	timestamps: {
 		createdAt: 'created_at',
