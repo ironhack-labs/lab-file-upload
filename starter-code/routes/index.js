f01eb073206316b471a3789d18c4d91852937950
lucas <cerfectus@gmail.com>
2018-10-23 17:28:05 -0500

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router  = express.Router();
-
-/* GET home page. */
+const User = require("../models/user");
+ function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/auth/login");
+}
+ /* GET home page */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  res.render('index');
 });
-
-module.exports = router;
+ router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', req.user)
+});
+ router.get('/profile/:id', (req, res) => {
+  User.findById(req.params.id)
+  .then(user => {
+    res.render('profile', {user})
+  })
+  .catch(err => {
+    console.log(err)
+  });
+});
+ module.exports = router;
