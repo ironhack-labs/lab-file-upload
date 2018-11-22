d7d118be0bc680c33ce0420e5c978d0d6340a591
RuddyLotar <ruddylotar@gmail.com>
2018-11-22 15:22:07 +0100

    image registration done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,6 +1,7 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const uploadCloud = require('../config/cloudinary.js');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
@@ -17,11 +18,11 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', [ensureLoggedOut(), uploadCloud.single('imgName')], passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
-}));
+}))
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
