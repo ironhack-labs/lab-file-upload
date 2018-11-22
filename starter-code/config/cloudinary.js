c43635b0cd8a1e0b658a8c45abb92bbc0a9c0bf9
Javier Samper <jsamperarias@hotmail.com>
2018-11-22 16:29:56 +0100

    Primera iteracion

---
+++ starter-code/config/cloudinary.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+const multer = require('multer');
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+});
+
+//cloudinaryStorage config. please see its API via https://www.npmjs.com/package/multer-storage-cloudinary
+var storage = cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'proyecto', // The name of the folder in cloudinary
+    allowedFormats: ['jpg', 'png'],
+    //remember you can create your custom filename pattern
+    filename: function(req, file, cb) {
+        cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
+    }
+});
+const uploadCloud = multer({ storage: storage });
+
+module.exports = uploadCloud;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,42 @@
 const express = require('express');
-const router  = express.Router();
+require('dotenv');
+const uploadCloud = require('../config/cloudinary.js')
+const router = express.Router();
+const User = require('../models/user.js');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+    User.find()
+        .then((images) => {
+            res.render('index', { images });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
 });
+
+
+//actual write to cloudinary via the middleware specified in ../config/cloudinary.js
+router.post('/img/add', uploadCloud.single('photo'), (req, res, next) => {
+    //write preparation, extracting the values send via the form
+    const { title, description } = req.body;
+    const imgPath = req.file.url;
+    const imgName = req.file.originalname;
+    const newImg = new User({ title, description, imgPath, imgName })
+        //actual write in mongo using mongoose
+    newImage.save()
+        .then(image => {
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
+
+
+
+
 
 module.exports = router;
