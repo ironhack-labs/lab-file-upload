4d89163ae3ba1f99b4e79eb47a92beabae243b57
Xiaomi Notebook Pro <xiaomi@gmailcom>
2018-11-22 15:44:11 +0100

    1 iteration done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,7 +1,11 @@
+require('dotenv');
+
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const uploadCloud = require('../config/cloudinary.js');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
@@ -17,7 +21,7 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', [ensureLoggedOut(), uploadCloud.single('photo')], passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
