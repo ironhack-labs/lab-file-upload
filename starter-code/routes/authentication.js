0fa654c66329247e6ed3e2e2c94dcb63363e917f
cocobilbao <cocobilbao@gmail.com>
2018-11-22 16:18:13 +0100

    iteration 1 with CDN

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,7 +1,10 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const User = require("../models/user");
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const uploadCloud = require('../config/cloudinary.js');
+const bcrypt = require("bcrypt");
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
@@ -17,11 +20,32 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', uploadCloud.single('photo'), ensureLoggedOut()/*, passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
-}));
+})*/, (req,res) => {
+    const { username, email, password } = req.body;
+              const imgPath = req.file.url;
+              const imgName = req.file.originalname;
+
+              const hashPass = bcrypt.hashSync(
+                password,
+                bcrypt.genSaltSync(8),
+                null
+              );
+              const newUser = new User({
+                username,
+                email,
+                password: hashPass,
+                imgName,
+                imgPath
+              });
+
+              newUser.save()
+              .then(()=>{console.log("Usuario guardado"); res.redirect("/login")})
+              .catch(err => console.log(err));
+});
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
