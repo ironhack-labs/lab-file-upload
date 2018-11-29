2b87ec4ed1bfc3eb6fa720c70497a576551c2ae9
aemartos <aemartos@gmail.com>
2018-11-29 22:12:14 +0100

    Add favicon and set partials

--- starter-code/app.js
+++ starter-code/app.js
@@ -115,7 +115,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
