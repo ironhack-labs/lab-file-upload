f68608a2067e18242194b7d309095c7030526d3b
Rene Manzano <rene@Renes-MacBook.local>
2018-11-29 19:56:01 -0600

    Done

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,7 +1,12 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+const multer     = require('multer')
+const upload   = multer ({dest:'./public/pics'})
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const Post = require('../models/post')
+const User = require('../models/user')
+const Comment = require('../models/comment')
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
@@ -33,5 +38,67 @@ router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
 });
+router.get('/new',ensureLoggedIn('/login'),(req,res,next)=>{
+    res.render('new')
+})
+router.post('/new',upload.single('photo'),(req,res,next)=>{
+    console.log(req.file)
+    const post = new Post({
+        content: req.body.content,
+        picPath: `/pics/${req.file.filename}`,
+        picName: req.file.originalname,
+        creatorId: req.user._id
+      });
+    Post.create(post)
+    .then(post=>{
+        res.redirect('/index')
+    })
+
+})
+router.get('/index',(req,res,next)=>{
+    var arreglo = []
+    Post.find()
+    .then(posts=>{
+       
+        res.render('list',{posts})
+    }).catch(e=>console.log(e))
+    
+})
+
+router.get('/comment/:id',ensureLoggedIn('/login'),(req,res,next)=>{
+    const {id} = req.params
+    const action = `/comment/${id}`
+    console.log(id)
+    res.render('comment',{action})
+})
+router.post('/comment/:id',upload.single('photo'),(req,res,next)=>{
+    const {id} = req.params
+    const comment = new Comment({
+        content: req.body.content,
+        imagePath: `/pics/${req.file.filename}`,
+        imageName: req.file.originalname,
+        authorId: req.user._id
+      });
+    Comment.create(comment)
+    .then(comment=>{
+        Post.findByIdAndUpdate(id,{$push:{commentId:comment._id}})
+        .then(post=>{
+            res.redirect(`/detail/${post._id}`)
+          }).catch(e=>next(e))
+          
+        }).catch(error=>{
+          console.log(error)
+        })
+    
+
+})
+router.get('/detail/:id',(req,res,next)=>{
+    const {id} = req.params
+    Post.findById(id).populate('commentId')
+  .then(post=>{
+    res.render('detail',post)
+  }).catch(e=>next(e))
+
+})
 
 module.exports = router;
