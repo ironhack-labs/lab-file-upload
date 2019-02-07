2fa2b436de5abcdfdf8525c257de32d9c1c128cb
Rojiblanco182 <robertosalinas1988@gmail.com>
2019-02-07 21:34:48 +0100

    done

---
+++ starter-code/models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = Schema({
+  content: String,
+  authorId: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  imagePath: String,
+  imageName: String
+})
+
+const PhotoComment = mongoose.model("Comment", commentSchema);
+module.exports = PhotoComment;
--- starter-code/routes/posts.js
+++ starter-code/routes/posts.js
@@ -2,14 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Post = require("../models/post")
 const cloudinary = require("../config/cloudinary")
+const PhotoComment = require("../models/comment")
+
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get("/", (req, res, next) => {
-  Post.find()
+  Post.find({})
+    .populate("comments")
     .then(posts => res.render("posts/list-post", { posts }))
     .catch(err => console.log(err))
 })
 
-router.get("/add", (req, res, next) => {
+router.get("/add", ensureLoggedIn('/login'), (req, res, next) => {
   res.render("posts/add-post")
 })
 
@@ -22,6 +26,25 @@ router.post("/add", cloudinary.single("photo"), (req, res, next) => {
   })
   newPost.save()
     .then(() => res.redirect("/posts/"))
+    .catch(err => console.log(err))
+})
+
+router.post("/add-comment/:id", cloudinary.single("photo"), (req, res, next) => {
+  const newComment = new PhotoComment({
+    content: req.body.content,
+    authorId: req.user._id,
+    imagePath: req.file.secure_url,
+    imageName: req.file.originalname
+  })
+  newComment.save()
+    .then(comment => {
+      Post.findByIdAndUpdate(req.params.id, { $push: { comments: comment._id } })
+        .then(() => {
+          console.log("the comment was published")
+          res.redirect("/posts/")
+        })
+        .catch(err => console.log(err))
+    })
     .catch(err => console.log(err))
 })
 
