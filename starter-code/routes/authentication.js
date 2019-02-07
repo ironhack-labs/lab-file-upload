c0d3ee56bf18fc7df59be36166b728784df9f2e0
Mr.X <seniorito.x@icloud.com>
2019-02-07 17:12:30 +0100

    Iteration 1 🤓

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,6 +1,14 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
+// const multer  = require('multer');
+const User = require('../models/user');
+const bcrypt= require('bcrypt')
+const cloudinary = require('../options/cloudinary')
+
+const salt= bcrypt.genSaltSync(10)
+
+
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
@@ -17,11 +25,20 @@ router.get('/signup', ensureLoggedOut(), (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
-}));
+router.post('/signup', cloudinary.single('photo'), (req, res,next) => {
+    const myUser = new User({
+        username: req.body.username,
+        email:    req.body.email,
+        password: bcrypt.hashSync(req.body.password, salt),
+        photoPath: req.file.secure_url,
+    })
+    myUser.save()
+    .then(payload => {
+        console.log('User was saved succesfully', payload)
+        res.redirect('/')
+    })
+    .catch(err => console.log('An error has ocurred while saving the user in the db', err))
+})
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
