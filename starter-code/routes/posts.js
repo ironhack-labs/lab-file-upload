48e124a59deb43e456a8ff32b287486f3b6832c8
caigma <caigma@gmail.com>
2019-02-08 09:37:54 +0100

    falta css y algunas validaciones de campos vacíos, cuando no subo fotos da error

--- starter-code/routes/posts.js
+++ starter-code/routes/posts.js
@@ -4,18 +4,22 @@ const express = require('express');
 const router = express.Router();
 const postModel = require('../models/post');
 const cloudinary = require('../options/cloudinary');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const commentModel = require('../models/comment');
 
 //en .find({}, {_id:0}) => estoy buscando todos los objetos de mi colleccion y le digo que no muestre el id
 router.get('/', (req, res, next) => {
 	postModel
-		.find({}, { _id: 0 })
+		.find({})
+		// la propiedad de Post comments:
+		.populate('comments')
 		.then((posts) => res.render('posts/list-post', { posts }))
 		.catch((err) => console.log('AN error ocurred finding post', err));
 });
 
 //realmente es /posts/add pero como he referenciado /posts como raiz no hace falta
 // de hecho debe coincidir con el action del FORM
-router.get('/add', (req, res, next) => {
+router.get('/add', ensureLoggedIn('/login'), (req, res, next) => {
 	res.render('posts/add-post');
 });
 
@@ -30,6 +34,28 @@ router.post('/add', cloudinary.single('photo'), (req, res, next) => {
 	});
 
 	newPost.save().then(() => res.redirect('/posts/')).catch((err) => console.log('An error ocurred saving', err));
+});
+
+router.post('/add-comment/:id', cloudinary.single('photo'), (req, res, next) => {
+	const newComment = new commentModel({
+		content: req.body.content,
+		authorId: req.user._id,
+		imagePath: req.file.secure_url,
+		iamge: req.file.originalname
+	});
+	newComment
+		.save()
+		//ME DEVUELVE EL ID DEL COMENTARIO QUE SE ACABA DE GENERAR Y GUARDAR
+		.then((comment) => {
+			postModel
+				.findByIdAndUpdate(req.params.id, {
+					//LENGUAJE MONGO...INSERTAMOS EL ID DEL COMENTARIO EN EL ARRAY DEL POST Y ACTUALIZANDO EL POST CON ESA INFO
+					$push: { comments: comment._id }
+				})
+				.then(() => console.log('A comment was saved succesfully'))
+				.catch((err) => console.log('An error ocurred refering a comment', err));
+		})
+		.catch((err) => console.log('An error ocurred saving a comment in db', err));
 });
 
 module.exports = router;
