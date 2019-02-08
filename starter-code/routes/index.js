6eb3e01557e905e45b32e13f34edf06b0a6744ce
Raphael Huber <raphael.ehuber@gmail.com>
2019-02-08 18:41:12 -0200

    picture uploaded and stored in the upload folder

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router  = express.Router();
+const multer  = require('multer');
+const Picture = require('../models/user');
+const upload = multer({ dest: './public/uploads/' });
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+router.get('/', function(req, res, next) {
+  Picture.find((err, pictures) => {
+    res.render('index', {pictures})
+  })
+});
+
+router.get('/upload', function(req, res, next) {
+  Picture.find((err, pictures) => {
+    res.render('upload')
+  })
+});
+
+router.post('/upload', upload.single('photo'), (req, res) => {
+
+  const pic = new Picture({
+    name: req.body.name,
+    path: `/uploads/${req.file.filename}`,
+    originalName: req.file.originalname
+  });
+
+  pic.save((err) => {
+      res.redirect('/');
+  });
 });
 
 module.exports = router;
