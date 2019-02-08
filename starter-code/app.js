ae01304bc3fa7666bd05cccb45c9bcf00b2d8769
Henrique Mendes <henrique.gmendes@hotmail.com>
2019-02-08 17:54:12 -0200

    [SAO FT2 - PP - W5D4] Henrique Mendes e Cristiana Yasuda

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,18 @@
-const express            = require('express');
-const path               = require('path');
-const favicon            = require('serve-favicon');
-const logger             = require('morgan');
-const cookieParser       = require('cookie-parser');
-const bodyParser         = require('body-parser');
-const passport           = require('passport');
-const LocalStrategy      = require('passport-local').Strategy;
-const User               = require('./models/user');
-const bcrypt             = require('bcrypt');
-const session            = require('express-session');
-const MongoStore         = require('connect-mongo')(session);
-const mongoose           = require('mongoose');
-const flash              = require('connect-flash');
-const hbs                = require('hbs')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const hbs = require('hbs');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
 
@@ -25,8 +25,8 @@ app.use(session({
   secret: 'tumblrlabdev',
   resave: false,
   saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
+  store: new MongoStore({ mongooseConnection: mongoose.connection })
+}));
 
 passport.serializeUser((user, cb) => {
   cb(null, user.id);
@@ -45,10 +45,10 @@ passport.use('local-login', new LocalStrategy((username, password, next) => {
       return next(err);
     }
     if (!user) {
-      return next(null, false, { message: "Incorrect username" });
+      return next(null, false, { message: 'Incorrect username' });
     }
     if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
+      return next(null, false, { message: 'Incorrect password' });
     }
 
     return next(null, user);
@@ -60,35 +60,36 @@ passport.use('local-signup', new LocalStrategy(
   (req, username, password, next) => {
     // To avoid race conditions
     process.nextTick(() => {
-        User.findOne({
-            'username': username
-        }, (err, user) => {
-            if (err){ return next(err); }
+      User.findOne({
+        username
+      }, (err, user) => {
+        if (err) { return next(err); }
 
-            if (user) {
-                return next(null, false);
-            } else {
-                // Destructure the body
-                const {
-                  username,
-                  email,
-                  password
-                } = req.body;
-                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-                const newUser = new User({
-                  username,
-                  email,
-                  password: hashPass
-                });
-
-                newUser.save((err) => {
-                    if (err){ next(null, false, { message: newUser.errors }) }
-                    return next(null, newUser);
-                });
-            }
+        if (user) {
+          return next(null, false);
+        }
+        // Destructure the body
+        const {
+          username,
+          email,
+          password
+        } = req.body;
+        const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+        const newUser = new User({
+          username,
+          email,
+          password: hashPass,
+          imgPath: ''
         });
+
+        newUser.save((err) => {
+          if (err) { next(null, false, { message: newUser.errors }); }
+          return next(null, newUser);
+        });
+      });
     });
-}));
+  }
+));
 
 app.use(flash());
 app.use(passport.initialize());
@@ -101,6 +102,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
+
 app.use('/', index);
 app.use('/', authRoutes);
 
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,37 +1,52 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
+const express = require('express');
+const passport = require('passport');
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const uploadCloud = require('../config/cloudinary.js');
+const User = require('./models/user');
+
+
+const router = express.Router();
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+  res.render('authentication/login', { message: req.flash('error') });
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
 }));
 
 router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+  res.render('authentication/signup', { message: req.flash('error') });
 });
 
 router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash: true
 }));
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-    res.render('authentication/profile', {
-        user : req.user
-    });
+  res.render('authentication/profile', {
+    user: req.user
+  });
+});
+
+router.get('/profile/update/:id', ensureLoggedIn('/login'), (req, res) => {
+  res.render('authentication/profile-update', {
+  });
+});
+
+router.post('/profile/update', ensureLoggedIn('/login'), (req, res) => {
+    const
+    res.render('authentication/profile-update', {
+  });
 });
 
 router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
-    req.logout();
-    res.redirect('/');
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
