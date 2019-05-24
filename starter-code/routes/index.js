9def79000e45bd74b3c085cee6585ce6eec27960
Tino Gutiérrez <tgf1987@gmail.com>
2019-05-24 09:53:37 +0200

    Comments

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,10 +1,10 @@
 const express    = require('express');
-const passport   = require('passport');
+// const passport   = require('passport');
 const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+// const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 const multer  = require('multer');
-const upload = multer({ dest: '../public/uploads/' });
-const uploadCloudinary = require("../config/cloudinary");
+// const upload = multer({ dest: '../public/uploads/' });
+// const uploadCloudinary = require("../config/cloudinary");
 const Post = require('../models/post');
 const Comment = require('../models/comment');
 
