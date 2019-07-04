918dc0293da2e2b231ee5d744dc6fbe00c573ba3
Iván López Ruiz <ivan@MacBook-Air-de-Ivan.local>
2019-07-04 16:54:24 +0200

    done

--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
-const UserSchema = Schema({
+const userSchema = new Schema({
   username: String,
-  email:    String,
   password: String
+}, {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
 });
 
-const User = mongoose.model('User', UserSchema);
-
+const User = mongoose.model('User', userSchema);
 module.exports = User;
