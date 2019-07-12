bc7de8a85fc9c70e9875de19e9ec4c3eae590e3e
MFFB <marcusffbrandao@hotmail.com>
2019-07-12 09:40:59 -0300

    done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router  = express.Router();
+const User = require('../models/user.js')
+const uploadCloud = require('../config/cloudinary.js');
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express - Generated with IronGenerator' });
+  User.find()
+  .then((user) => {
+    res.render('index', { user });
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 });
 
 module.exports = router;
