23972cbef0a01c97e216626c7fb809da9fc827b1
Alex Cabo Martinez <alex@MacBook-Air-de-Alex.local>
2019-08-02 10:59:44 +0200

    tumblr

---
+++ starter-code/models/post.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = Schema({
+    title: String
+
+})
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,51 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const multer = require('multer')
+
+const Post = require("../models/post.model")
+
+const uploadCloud = require('../configs/cloudinary.config')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
-});
+  Post.find()
+    .then(posts => {
+      res.render('index', { posts })
+    })
+
+})
+
+router.post('/new-post', (req, res, next) => {
+
+  const title = req.body.title
+
+
+  Post.create({ title })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log('Hubo un error:', err))
+})
 
 module.exports = router;
+
+
+// llamar a la url del action en la ruta del index  router.post('/new-post') 
+// cambiar input form name = tittle 
+
+// req recibe url 
+// res  repsose
+
+//en req.body estara el formulario  -> req.body.title = input (el post) 
+
+// crear coleccion en mongoos para los posts || crear modelo -> requiere('ruta MODELO POST')
+
+// ruta || metger cloudinary
+// post.create({title: req.body.title})
+//.then(post => {
+//res.redirect('/')
+//})
+
+//modificar este render
+//Post.find()
+//.then (posts => {
+//res.resnder('index', {posts})
+//})
