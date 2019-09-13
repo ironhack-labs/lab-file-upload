189f26ebbc9f7e4eddec8d7211465aa35f20f6dc
Daniel Garcia <jdanielink@gmail.com>
2019-09-12 23:01:55 -0500

    done :octocat:

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,98 +1,34 @@
-const express            = require('express');
-const path               = require('path');
-const favicon            = require('serve-favicon');
-const logger             = require('morgan');
-const cookieParser       = require('cookie-parser');
-const bodyParser         = require('body-parser');
-const passport           = require('passport');
-const LocalStrategy      = require('passport-local').Strategy;
-const User               = require('./models/user');
-const bcrypt             = require('bcrypt');
-const session            = require('express-session');
-const MongoStore         = require('connect-mongo')(session);
-const mongoose           = require('mongoose');
-const flash              = require('connect-flash');
-const hbs                = require('hbs')
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('./config/passport');
 
-mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
+mongoose.connect(process.env.DB);
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(session({
-  secret: 'tumblrlabdev',
-  resave: false,
-  saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
-}))
+app.use(
+  session({
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24
+    },
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
-passport.serializeUser((user, cb) => {
-  cb(null, user.id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-passport.use('local-login', new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
-}));
-
-passport.use('local-signup', new LocalStrategy(
-  { passReqToCallback: true },
-  (req, username, password, next) => {
-    // To avoid race conditions
-    process.nextTick(() => {
-        User.findOne({
-            'username': username
-        }, (err, user) => {
-            if (err){ return next(err); }
-
-            if (user) {
-                return next(null, false);
-            } else {
-                // Destructure the body
-                const {
-                  username,
-                  email,
-                  password
-                } = req.body;
-                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-                const newUser = new User({
-                  username,
-                  email,
-                  password: hashPass
-                });
-
-                newUser.save((err) => {
-                    if (err){ next(null, false, { message: newUser.errors }) }
-                    return next(null, newUser);
-                });
-            }
-        });
-    });
-}));
-
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -100,9 +36,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 const index = require('./routes/index');
-const authRoutes = require('./routes/authentication');
 app.use('/', index);
-app.use('/', authRoutes);
+app.use('/', require('./routes/auth'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const uploadCloud = require('./../config/multer');
+const { catchErrors, isLoggedIn } = require('./../middleware/');
+const {
+  uploadProfileImage,
+  getAllPosts,
+  getOnePost,
+  getPostForm,
+  createPost,
+  createComment
+} = require('./../controllers/index.controller');
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
-});
+router.get('/', catchErrors(getAllPosts));
+
+router.post('/user/:id/upload-profile', uploadCloud.single('profile'), catchErrors(uploadProfileImage));
+
+router.get('/post/add', isLoggedIn, getPostForm);
+router.post('/post/add', isLoggedIn, uploadCloud.single('picture'), createPost);
+router.get('/post/:id', catchErrors(getOnePost));
+
+router.post('/post/:id/add-comment', uploadCloud.single('picture'), isLoggedIn, createComment);
 
 module.exports = router;
