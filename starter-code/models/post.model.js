2edd8093e4000f79d4ccf06a38281597ea7e05a2
Rebecca Wood <rebecca@Rebeccas-Air.home>
2019-11-21 21:05:47 +0100

    subdoc model

--- starter-code/models/post.model.js
+++ starter-code/models/post.model.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const PostSchema = Schema({
-    content:String,
+const commentSchema = Schema({
+    content: String,
     imgPath: String,
     imgName: String,
+});
+
+const PostSchema = Schema({
+    content: String,
+    imgPath: String,
+    imgName: String,
+    comment: [commentSchema]
 });
 const Post = mongoose.model('Post', PostSchema);
 
