2460244636e54a0faa9e00ff76b12a005c8434d0
estefania-git <ecasmed@gmail.com>
2019-11-21 20:13:55 +0100

    first commit

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,18 +1,18 @@
-const express            = require('express');
-const path               = require('path');
-const favicon            = require('serve-favicon');
-const logger             = require('morgan');
-const cookieParser       = require('cookie-parser');
-const bodyParser         = require('body-parser');
-const passport           = require('passport');
-const LocalStrategy      = require('passport-local').Strategy;
-const User               = require('./models/user');
-const bcrypt             = require('bcrypt');
-const session            = require('express-session');
-const MongoStore         = require('connect-mongo')(session);
-const mongoose           = require('mongoose');
-const flash              = require('connect-flash');
-const hbs                = require('hbs')
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const hbs = require('hbs')
 
 mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
 
@@ -25,7 +25,9 @@ app.use(session({
   secret: 'tumblrlabdev',
   resave: false,
   saveUninitialized: true,
-  store: new MongoStore( { mongooseConnection: mongoose.connection })
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection
+  })
 }))
 
 passport.serializeUser((user, cb) => {
@@ -34,68 +36,89 @@ passport.serializeUser((user, cb) => {
 
 passport.deserializeUser((id, cb) => {
   User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
+    if (err) {
+      return cb(err);
+    }
     cb(null, user);
   });
 });
 
 passport.use('local-login', new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
+  User.findOne({
+    username
+  }, (err, user) => {
     if (err) {
       return next(err);
     }
     if (!user) {
-      return next(null, false, { message: "Incorrect username" });
+      return next(null, false, {
+        message: "Incorrect username"
+      });
     }
     if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
+      return next(null, false, {
+        message: "Incorrect password"
+      });
     }
 
     return next(null, user);
   });
 }));
 
-passport.use('local-signup', new LocalStrategy(
-  { passReqToCallback: true },
+passport.use('local-signup', new LocalStrategy({
+    passReqToCallback: true
+  },
   (req, username, password, next) => {
     // To avoid race conditions
     process.nextTick(() => {
-        User.findOne({
-            'username': username
-        }, (err, user) => {
-            if (err){ return next(err); }
+      User.findOne({
+        'username': username
+      }, (err, user) => {
+        if (err) {
+          return next(err);
+        }
 
-            if (user) {
-                return next(null, false);
-            } else {
-                // Destructure the body
-                const {
-                  username,
-                  email,
-                  password
-                } = req.body;
-                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-                const newUser = new User({
-                  username,
-                  email,
-                  password: hashPass
-                });
+        if (user) {
+          return next(null, false);
+        } else {
+          // Destructure the body
+          const {
+            username,
+            email,
+            password
+          } = req.body;
+          const photoUrl = req.file.url;
+          const photoName = req.file.originalname;
+          const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+          const newUser = new User({
+            username,
+            email,
+            password: hashPass,
+            photoUrl,
+            photoName
+          });
 
-                newUser.save((err) => {
-                    if (err){ next(null, false, { message: newUser.errors }) }
-                    return next(null, newUser);
-                });
+          newUser.save((err) => {
+            if (err) {
+              next(null, false, {
+                message: newUser.errors
+              })
             }
-        });
+            return next(null, newUser);
+          });
+        }
+      });
     });
-}));
+  }));
 
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- starter-code/models/user.js
+++ starter-code/models/user.js
@@ -1,12 +1,20 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const UserSchema = Schema({
   username: String,
-  email:    String,
-  password: String
+  email: String,
+  password: String,
+  photoName: {
+    type: String,
+    defaul: "hola"
+  },
+  photoUrl: {
+    type: String,
+    default: "adios"
+  }
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 module.exports = User;
