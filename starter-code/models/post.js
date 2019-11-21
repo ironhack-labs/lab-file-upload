668988713b2d1414d099ca7192a999c85c3d73ba
gabzafra <gab.zafra@gmail.com>
2019-11-21 21:39:02 +0100

    iteration 2

---
+++ starter-code/models/post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const PostSchema = Schema({
+  creatorId: String,
+  content: String,
+  imgURL: String,
+  imgName: String
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,39 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
+const Post = require('../models/post');
+const uploadCloud = require('../config/cloudinary');
+const {
+  ensureLoggedIn,
+  ensureLoggedOut
+} = require('connect-ensure-login');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express - Generated with IronGenerator' });
+  Post.find().then(posts => {
+    res.render('index', {
+      title: 'Express - Generated with IronGenerator',
+      posts
+    })
+  })
 });
+
+router.get('/new', ensureLoggedIn('/login'), (req, res, next) => {
+  res.render('new', {
+    title: 'Express - Generated with IronGenerator'
+  });
+});
+
+router.post('/new', uploadCloud.single('image'), ensureLoggedIn('/login'), (req, res, next) => {
+  Post.create({
+    creatorId: req.user.id,
+    content: req.body.content,
+    imgURL: req.file.url,
+    imgName: req.file.originalname
+  }).then(()=>res.redirect('/'))
+});
+
+
+
+
 
 module.exports = router;
