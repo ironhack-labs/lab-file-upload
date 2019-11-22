23f0dd91e3f7e1b8c094a5a787cb667cdad374dc
Jessica <54844929+jessica-git@users.noreply.github.com>
2019-11-22 09:44:46 +0100

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -14,6 +14,8 @@ const mongoose           = require('mongoose');
 const flash              = require('connect-flash');
 const hbs                = require('hbs')
 
+require("dotenv").config()
+
 mongoose.connect('mongodb://localhost:27017/tumblr-lab-development');
 
 const app = express();
@@ -74,11 +76,17 @@ passport.use('local-signup', new LocalStrategy(
                   email,
                   password
                 } = req.body;
+
+                const imgPath = req.file.url
+                const imgName = req.file.originalname
+                
                 const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
                 const newUser = new User({
                   username,
                   email,
-                  password: hashPass
+                  password: hashPass,
+                  imgPath: imgPath ,
+                  imgName: imgName 
                 });
 
                 newUser.save((err) => {
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,37 +1,68 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const uploadCloud = require("../configs/cloudinary")
+const multer = require('multer')
+const Picture = require('../models/user')
+const upload = multer({ dest: './public/uploads/' })
+
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
+
+//INICIO
+router.get('/', (req, res) => {
+    Picture.find()
+        .then(allPictures => res.render("images/index", { pictures: allPictures }))
+        .catch(err => console.log(err))
+})
+
+
+//SINGUP-REGISTRO
+router.get('/signup', ensureLoggedOut(), (req, res) => {
+    res.render('authentication/signup', { message: req.flash('error') });
+});
+
+router.post('/signup', ensureLoggedOut(), uploadCloud.single("imgFile"), passport.authenticate('local-signup', {
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
+}));
+
+//LOGIN-INICIAR SESIÓN
 router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+    res.render('authentication/login', { message: req.flash('error') });
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
 }));
 
-router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
-});
-
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
-}));
-
+//PERFIL
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
-        user : req.user
+        user: req.user
     });
 });
 
+//LOGOUT.CERRAR SESIÓN
 router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
     req.logout();
     res.redirect('/');
 });
+
+
+//UPLOAD-SUBIDA IMAGEN
+router.get('/upload', (req, res) => res.render("images/upload"))
+
+router.post('/upload', uploadCloud.single('imgFile'), (req, res) => { 
+
+    console.log('La información que MULTER deja en req.file es:', req.file)
+
+    Picture.create({ name: req.body.imgName, path: `/uploads/${req.file.filename}`, originalName: req.file.originalname })
+        .then(() => res.redirect('/images'))
+        .catch(err => console.log(err))
+})
 
 module.exports = router;
