687a98f000028aedb627b6e495dd78a95589aecc
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-11-22 09:12:49 +0100

    hay cositas

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,32 +1,49 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const User = require('../models/user')
+const multer = require('multer');
+const uploadCloud = require('../configs/cloudinary.config');
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+    res.render('authentication/login', { message: req.flash('error') });
 });
 
 router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
 }));
 
 router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+    res.render('authentication/signup', { message: req.flash('error') });
 });
 
 router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
+    successRedirect: '/',
+    failureRedirect: '/signup',
+    failureFlash: true
 }));
 
 router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
     res.render('authentication/profile', {
-        user : req.user
+        user: req.user
     });
+});
+router.get('/images/upload', (req, res) => {
+    res.render('images/upload', {
+        user: req.user
+    });
+});
+router.post('/images/upload', uploadCloud.single('imgFile'), (req, res) => {
+    const imgPath = req.file.url
+    const imgName = req.body
+    User.findbyIdandUpdate(req.user_id, { imgPath, imgName })
+        .then(user => res.redirect('/'))
+        .catch(error => console.log(error))
+
 });
 
 router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
