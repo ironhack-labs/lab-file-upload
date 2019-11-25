d32b00a4593d6889daef95d161786fb3387dd501
merrymustard <merryymustard@gmail.com>
2019-11-24 23:10:30 -0600

    make it better

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post.js');
-// const User = require('../models/User.js');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -13,8 +12,3 @@ router.get('/', async (req, res, next) => {
 
 
 module.exports = router;
-
-// exports.feedGet = async (req, res, next) => {
-//   const posts = await Post.find().populate("creatorId");
-//   res.render("index", { posts });
-// };
