ee4767eb7bae180d2127e635794937470a926972
Berna <berna@email.com>
2019-11-25 02:35:47 -0600

    Falta deploy

--- starter-code/controllers/comments.controller.js
+++ starter-code/controllers/comments.controller.js
@@ -3,7 +3,21 @@ const User = require('../models/User');
 const Comment = require('../models/Comment');
 
 exports.newCommentView = (req, res) => {
-	res.render('comments/new');
+	const { id } = req.params;
+	console.log(id);
+	res.render('comments/new', { id });
 };
 
-exports.newCommentProcess = (req, res) => {};
+exports.newCommentProcess = (req, res) => {
+	const { content, imgName } = req.body;
+	const { secure_url } = req.file ? req.file : 'withoutimage';
+	const { id } = req.params;
+	const creatorId = req.user;
+	Comment.create({ content, authorId: creatorId, imgPath: secure_url, imgName: imgName ? imgName : 'withoutimage' })
+		.then((comment) => {
+			Post.findByIdAndUpdate(id, { $push: { comments: comment._id } })
+				.then((post) => res.redirect(`/posts`))
+				.catch((err) => console.log(err));
+		})
+		.catch((err) => console.log(err));
+};
--- starter-code/routes/posts.js
+++ starter-code/routes/posts.js
@@ -11,7 +11,7 @@ router.get('/', postsView);
 router.get('/new', newPostView);
 router.post('/new', ensureLoggedIn('/login'), uploadCloud.single('photo'), newPostProcess);
 
-router.get('/post/:id', newCommentView);
-router.get('/post/:id/newComment', newCommentProcess);
+router.get('/:id', newCommentView);
+router.post('/:id/newComment', ensureLoggedIn('/login'), uploadCloud.single('photo'), newCommentProcess);
 
 module.exports = router;
