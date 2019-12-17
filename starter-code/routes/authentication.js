8ce5c54628f8a2cf267ee686211f81ed9eac23c4
abernier <antoine.bernier@gmail.com>
2019-12-17 14:23:48 +0100

    -connect-ensure-login

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,35 +1,39 @@
 const express    = require('express');
 const passport   = require('passport');
 const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
-router.get('/login', ensureLoggedOut(), (req, res) => {
+router.get('/login', (req, res) => {
     res.render('authentication/login', { message: req.flash('error')});
 });
 
-router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
+router.post('/login', passport.authenticate('local-login', {
   successRedirect : '/',
   failureRedirect : '/login',
   failureFlash : true
 }));
 
-router.get('/signup', ensureLoggedOut(), (req, res) => {
+router.get('/signup', (req, res) => {
     res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+router.post('/signup', passport.authenticate('local-signup', {
   successRedirect : '/',
   failureRedirect : '/signup',
   failureFlash : true
 }));
 
-router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
+router.get('/profile', (req, res) => {
+    if (!req.user) {
+        res.redirect('/login');
+        return;
+    }
+
     res.render('authentication/profile', {
         user : req.user
     });
 });
 
-router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
+router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
