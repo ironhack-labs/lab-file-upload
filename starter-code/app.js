eb9be9fa737404f9949c0526e592fbf8d8c3f90a
abernier <antoine.bernier@gmail.com>
2019-12-17 15:01:08 +0100

    making the LAB more ISO with the lesson

--- starter-code/app.js
+++ starter-code/app.js
@@ -28,68 +28,6 @@ app.use(session({
   store: new MongoStore( { mongooseConnection: mongoose.connection })
 }))
 
-passport.serializeUser((user, cb) => {
-  cb(null, user.id);
-});
-
-passport.deserializeUser((id, cb) => {
-  User.findById(id, (err, user) => {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
-});
-
-passport.use('local-login', new LocalStrategy((username, password, next) => {
-  User.findOne({ username }, (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return next(null, false, { message: "Incorrect username" });
-    }
-    if (!bcrypt.compareSync(password, user.password)) {
-      return next(null, false, { message: "Incorrect password" });
-    }
-
-    return next(null, user);
-  });
-}));
-
-passport.use('local-signup', new LocalStrategy(
-  { passReqToCallback: true },
-  (req, username, password, next) => {
-    // To avoid race conditions
-    process.nextTick(() => {
-        User.findOne({
-            'username': username
-        }, (err, user) => {
-            if (err){ return next(err); }
-
-            if (user) {
-                return next(null, false);
-            } else {
-                // Destructure the body
-                const {
-                  username,
-                  email,
-                  password
-                } = req.body;
-                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-                const newUser = new User({
-                  username,
-                  email,
-                  password: hashPass
-                });
-
-                newUser.save((err) => {
-                    if (err){ next(null, false, { message: newUser.errors }) }
-                    return next(null, newUser);
-                });
-            }
-        });
-    });
-}));
-
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -98,6 +36,40 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  User.findById(id)
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+  ;
+});
+
+passport.use(new LocalStrategy(
+  {passReqToCallback: true},
+  (...args) => {
+    const [req,,, done] = args;
+
+    const {username, password} = req.body;
+
+    User.findOne({username})
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+          
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, { message: "Incorrect password" });
+        }
+    
+        done(null, user);
+      })
+      .catch(err => done(err))
+    ;
+  }
+));
 
 const index = require('./routes/index');
 const authRoutes = require('./routes/authentication');
--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,36 +1,83 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
+const express = require('express');
+const router = express.Router();
 
-router.get('/login', (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+const passport = require('passport');
+const User = require('../models/user.js');
+
+const bcrypt = require("bcrypt");
+const bcryptSalt = 10;
+
+router.get('/signup', (req, res) => {
+  res.render('authentication/signup', { message: req.flash('error')});
 });
 
-router.post('/login', passport.authenticate('local-login', {
+router.post("/signup", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // 1. Check username and password are not empty
+  if (username === "" || password === "") {
+    res.render("authentication/signup", { errorMessage: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then(user => {
+      // 2. Check user does not already exist
+      if (user) {
+        res.render("authentication/signup", { errorMessage: "The username already exists" });
+        return;
+      }
+
+      // Encrypt the password
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
+
+      //
+      // Save the user in DB
+      //
+
+      const newUser = new User({
+        username,
+        password: hashPass
+      });
+
+      newUser.save()
+        .then(user => {
+          // save user in session: req.user
+          req.login(user, err => {
+            if (err) return next(err); // Session save went bad
+
+            res.redirect('/'); // All good, we are now logged in and `req.user` is now set
+          });
+        })
+        .catch(err => next(err))
+      ;
+        
+    })
+    .catch(err => next(err))
+  ;
+});
+
+router.get('/login', (req, res) => {
+  res.render('authentication/login', { message: req.flash('error')});
+});
+
+router.post('/login', passport.authenticate('local', {
   successRedirect : '/',
   failureRedirect : '/login',
   failureFlash : true
 }));
 
-router.get('/signup', (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
-});
-
-router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
-}));
-
 router.get('/profile', (req, res) => {
-    if (!req.user) {
-        res.redirect('/login');
-        return;
-    }
+  if (!req.user) {
+    res.redirect('/login');
+    return;
+  }
 
-    res.render('authentication/profile', {
-        user : req.user
-    });
+  res.render('authentication/profile', {
+    user : req.user
+  });
 });
 
 router.get('/logout', (req, res) => {
