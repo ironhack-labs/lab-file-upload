cadfefef53f2365840581a66ea012df15199a52e
Juan Carlos <jcarlosriego@gmail.com>
2020-02-13 18:27:26 +0100

    Done

--- starter-code/routes/auth.routes.js
+++ starter-code/routes/auth.routes.js
@@ -1,7 +1,11 @@
+require(".dotenv").config();
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const uploadCloud = require('../configs/cloudinary.js');
+const User = require('../models/User.model')
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
   res.render('authentication/login', { message: req.flash('error') });
@@ -37,9 +41,31 @@ router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
   });
 });
 
+router.post('/photo/add:id', uploadCloud.single('photo'), (req, res, next) => {
+    
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+
+  let id = req.params.id
+  let user = {}
+
+  User.findOneAndUpdate(id).then
+  newMovie.save()
+  .then(movie => {
+    res.redirect('/');
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+
+z
 router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
   req.logout();
   res.redirect('/');
 });
+
+
 
 module.exports = router;
