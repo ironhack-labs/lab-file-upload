2e912f6318d8d925365648276710e63f6f13a7ed
Arturo G. Bruno <arturogbruno@gmail.com>
2020-02-13 18:09:58 +0100

    photo upload

--- starter-code/models/User.model.js
+++ starter-code/models/User.model.js
@@ -4,7 +4,9 @@ const { Schema, model } = mongoose;
 const UserSchema = new Schema({
   username: String,
   email: String,
-  password: String
+  password: String,
+  imgName: String,
+  imgPath: String,
 });
 
 module.exports = model('User', UserSchema);
--- starter-code/routes/auth.routes.js
+++ starter-code/routes/auth.routes.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
+
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const uploadCloud = require('../configs/cloudinary.js');
+const User = require('../models/User.model');
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
   res.render('authentication/login', { message: req.flash('error') });
@@ -41,5 +45,19 @@ router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
   req.logout();
   res.redirect('/');
 });
+
+router.post('/photo/add', uploadCloud.single('photo'), (req, res, next) => {
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+  User.findOneAndUpdate({ _id: req.user._id }, { $set: {imgName: imgName, imgPath: imgPath }})
+  .then(() => {
+    res.redirect('/profile');
+  })
+  .catch(error => {
+    console.log(error);
+  })
+});
+
+module.exports = router;
 
 module.exports = router;
