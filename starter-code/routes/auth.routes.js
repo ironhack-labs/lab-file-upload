eccf9a9c2ec1bbac7e13e6da6f4ddaf938901097
luciaoSainz <danielsainz28@gmail.com>
2020-02-14 09:28:16 +0100

    first

--- starter-code/routes/auth.routes.js
+++ starter-code/routes/auth.routes.js
@@ -1,41 +1,100 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+
+
+const uploadCloud = require('../configs/cloudinary.js');
+const {
+  ensureLoggedIn,
+  ensureLoggedOut
+} = require('connect-ensure-login');
+const User = require('../models/User.model')
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
+
 
 router.get('/login', ensureLoggedOut(), (req, res) => {
   res.render('authentication/login', { message: req.flash('error') });
+  res.render('authentication/login', {
+    message: req.flash('error')
+  });
 });
 
 router.post(
-  '/login',
-  ensureLoggedOut(),
-  passport.authenticate('local-login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash: true
-  })
+@@ -18,7 +28,9 @@ router.post(
 );
 
 router.get('/signup', ensureLoggedOut(), (req, res) => {
   res.render('authentication/signup', { message: req.flash('error') });
+  res.render('authentication/signup', {
+    message: req.flash('error')
+  });
 });
 
 router.post(
-  '/signup',
-  ensureLoggedOut(),
-  passport.authenticate('local-signup', {
-    successRedirect: '/profile',
-    failureRedirect: '/signup',
-    failureFlash: true
-  })
-);
-
-router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-  res.render('authentication/profile', {
-    user: req.user
+@@ -37,9 +49,71 @@ router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
   });
 });
+
+// router.post('/photo/add/:id', uploadCloud.single('photo'), (req, res, next) => {
+//   //write preparation, extracting the values send via the form
+//   const imgPath = req.file.url;
+//   const imgName = req.file.originalname;
+
+
+
+//   let id = req.params.id
+//   let user = { imgPath , imgName}
+
+//   User.findByIdAndUpdate(id, user)
+
+//     .then(updatedUser => {
+//       res.render('authentication/profile', {
+//         updatedUser
+//       });
+//     });
+
+// });
+
+
+router.post('/photo/add/:id', uploadCloud.single('photo'), (req, res, next) => {
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+
+  let id = req.params.id
+  let user = {
+    imgPath,
+    imgName
+  }
+
+  User.findByIdAndUpdate(req.user.id, user)
+    .then(updatedUser => {
+      res.render('authentication/profile', {
+        updatedUser
+      });
+    });
+
+
+});
+
+// router.post('/photo/add/:id', uploadCloud.single('photo'), (req, res, next) => {
+
+// const imgPath = req.file.url;
+// const imgName = req.file.originalname;
+
+// let id = req.params.id
+// let user = { imgPath , imgName}
+
+// User.findByIdAndUpdate(req.user.id, user)
+//   .then(updatedUser => {
+//     res.render('authentication/profile', {
+//       updatedUser
+//     });
+//   });
+
+// });
+
+
 
 router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
   req.logout();
@@ -43,3 +102,6 @@ router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
 });
 
 module.exports = router;
+
+
+module.exports = router; 
