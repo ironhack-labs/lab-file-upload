b857ce43bd23f57dec397aed88b213f005bdd569
Elvira Ramírez <elvirarp92@gmail.com>
2020-04-23 16:53:51 +0200

    create Image model for image uploading with Multer

---
+++ starter-code/models/Image.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const imageSchema = new Schema(
+  {
+    name: String,
+    path: String,
+    originalName: String,
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: true },
+  }
+)
+
+const Image = mongoose.model('Image', imageSchema)
+
+module.exports = Image
