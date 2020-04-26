91904d63a4e01948d0ef834308bc9bb9ff840161
edu93 <mailing_93@hotmail.com>
2020-04-25 22:51:13 -0500

    SemiDone

--- starter-code/controllers/post.controllers.js
+++ starter-code/controllers/post.controllers.js
@@ -6,25 +6,29 @@ exports.newPost = (req, res) => {
 }
 
 exports.newPostProcess = async (req, res) => {
-  const { content, user} = req.body
-  const { url : picPath, originalname : picName } = req.file
+  const { content, user } = req.body
+  const { url: picPath, originalname: picName } = req.file
 
-  const newPost = await Post.create({ content, user, picName, picPath, creatorId : req.user.id })
+  const newPost = await Post.create({ content, user, picName, picPath, creatorId: req.user.id })
 
   res.redirect('/posts')
 }
 
 exports.postsView = async (req, res) => {
-  const posts = await Post.find().populate("creatorId")
+  const posts = await Post.find().populate('creatorId')
   res.render('post/posts', { posts })
 }
 
+exports.newCommentProcess = async (req, res) => {
+  const { content, user } = req.body
+  const { url: imagePath, originalname: imageName } = req.file
+  const { id } = req.user
 
-exports.newCommentProcess = async(req, res) => {
-  const { content, user} = req.body
-  const { url : picPath, originalname : picName } = req.file
-
-  const newComment = await Post.findOneAndUpdate()
+  const newComment = await Post.findByIdAndUpdate(
+    id,
+    { comment: content, user, imageName, imagePath },
+    { new: true }
+  )
 
   res.redirect('/posts')
 }
