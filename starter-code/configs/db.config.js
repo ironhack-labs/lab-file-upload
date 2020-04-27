62a25c026272bb93b6562d4f2c358753becf6673
Kevin McCallum <kevingmcc@gmail.com>
2020-04-26 22:35:53 -0500

    mongo atlas config

--- starter-code/configs/db.config.js
+++ starter-code/configs/db.config.js
@@ -1,10 +1,11 @@
-const mongoose = require('mongoose');
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 mongoose
-  .connect('mongodb://localhost/tumblr-lab-dev', {
+  .connect(process.env.DB, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
-  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch(err => console.error('Error connecting to mongo', err));
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error('Error connecting to mongo', err))
