845f9cf1e835dfcf9e697a4bf0cb592b1c4ac242
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-03 13:34:49 -0500

    iteration 1 done

---
+++ configs/cloudinary.js
@@ -0,0 +1,20 @@
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const multer = require('multer')
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: (req, file) => ({
+        folder: "lab-file-upload",
+        allowed_formats: ['jpg', 'png'],
+        public_id: `app-${file.originalname}`
+    })
+})
+
+module.exports = multer({ storage })
