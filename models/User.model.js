d2f69863c34360f11972d13f90d08226645608e4
ak <konarayhan@gmail.com>
2020-11-12 13:39:34 -0500

    Iteracion 1

--- models/User.model.js
+++ models/User.model.js
@@ -22,6 +22,10 @@ const userSchema = new Schema(
     passwordHash: {
       type: String,
       required: [true, 'Password is required.']
+    },
+    //ITERACION 1: User Profile Picture
+    imageUrl: {
+      type: String
     }
   },
   {
