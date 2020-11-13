43ad6250b76144437c9ae3b38727339ad9ba4509
Gabriela Polanco <gabrielapolancoferreyra@gmail.com>
2020-11-12 21:22:57 -0500

    iteracion 2

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
+const mongoose = require('mongoose');
+const fileUploader = require('../configs/cloudinary.config')
+
 
 /* GET home page */
 router.get('/', (req, res) => res.render('index', { title: 'App created with Ironhack generator 🚀' }));
+
+router.get('/post-form', (req,res) => {
+  res.render('post-form')
+})
+
+router.post('/post-form', fileUploader.single('image'), (req,res) => {
+const { picName, creatorId, content } = req.body
+const picPath = req.file.path
+Post.create({content, creatorId, picPath , picName })
+.then(() => {
+  res.redirect('/posts')
+})
+
+})
+
+router.get('/posts', (req,res) => {
+Post.find()
+  .then(posts => {
+    console.log(posts)
+  res.render('posts', {posts})
+})
+})
+
+router.get('/post-details', (req,res) => {
+  res.render('post-details')
+})
+
 
 module.exports = router;
