a0efc133c2958accd492eea135edfac4e4829d56
sandrabosk <sandraboskovic@hotmail.com>
2021-05-26 03:00:59 +0200

    review and make minor updates

--- models/User.model.js
+++ models/User.model.js
@@ -1,27 +1,27 @@
 // models/User.model.js
 
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
     username: {
       type: String,
       trim: true,
-      required: [true, 'Username is required.'],
+      required: [true, "Username is required."],
       unique: true
     },
     email: {
       type: String,
-      required: [true, 'Email is required.'],
+      required: [true, "Email is required."],
       // this match will disqualify all the emails with accidental empty spaces, missing dots in front of (.)com and the ones with no domain at all
-      match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address.'],
+      match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
       unique: true,
       lowercase: true,
       trim: true
     },
     passwordHash: {
       type: String,
-      required: [true, 'Password is required.']
+      required: [true, "Password is required."]
     }
   },
   {
@@ -29,4 +29,4 @@ const userSchema = new Schema(
   }
 );
 
-module.exports = model('User', userSchema);
+module.exports = model("User", userSchema);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,48 +1,50 @@
 const router = require("express").Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // ℹ️ Handles password encryption
-const bcryptjs = require('bcryptjs');
+const bcryptjs = require("bcryptjs");
 
 // How many rounds should bcrypt run the salt (default [10 - 12 rounds])
 const saltRounds = 10;
 
 // Require the User model in order to interact with the database
-const User = require('../models/User.model');
+const User = require("../models/User.model");
 
-// Require necessary middlewares in order to control access to specific routes
-const shouldNotBeLoggedIn = require("../middlewares/shouldNotBeLoggedIn");
-const isLoggedIn = require("../middlewares/isLoggedIn");
+// require (import) middleware functions
+const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
 
 ////////////////////////////////////////////////////////////////////////
 ///////////////////////////// SIGNUP //////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
 // .get() route ==> to display the signup form to users
-router.get('/signup', shouldNotBeLoggedIn, (req, res) => res.render('auth/signup'));
+router.get("/signup", isLoggedOut, (req, res) => res.render("auth/signup"));
 
 // .post() route ==> to process form data
-router.post('/signup', shouldNotBeLoggedIn, (req, res, next) => {
+router.post("/signup", isLoggedOut, (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (!username || !email || !password) {
-    res.render('auth/signup', { errorMessage: 'All fields are mandatory. Please provide your username, email and password.' });
+    res.render("auth/signup", {
+      errorMessage: "All fields are mandatory. Please provide your username, email and password."
+    });
     return;
   }
 
   // make sure passwords are strong:
   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
   if (!regex.test(password)) {
-    res
-      .status(500)
-      .render('auth/signup', { errorMessage: 'Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter.' });
+    res.status(500).render("auth/signup", {
+      errorMessage:
+        "Password needs to have at least 6 chars and must contain at least one number, one lowercase and one uppercase letter."
+    });
     return;
   }
 
   bcryptjs
     .genSalt(saltRounds)
-    .then(salt => bcryptjs.hash(password, salt))
-    .then(hashedPassword => {
+    .then((salt) => bcryptjs.hash(password, salt))
+    .then((hashedPassword) => {
       return User.create({
         username,
         email,
@@ -52,16 +54,16 @@ router.post('/signup', shouldNotBeLoggedIn, (req, res, next) => {
         passwordHash: hashedPassword
       });
     })
-    .then(userFromDB => {
-      console.log('Newly created user is: ', userFromDB);
-      res.redirect('/user-profile');
+    .then((userFromDB) => {
+      // console.log("Newly created user is: ", userFromDB);
+      res.redirect("/user-profile");
     })
-    .catch(error => {
+    .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(500).render('auth/signup', { errorMessage: error.message });
+        res.status(500).render("auth/signup", { errorMessage: error.message });
       } else if (error.code === 11000) {
-        res.status(500).render('auth/signup', {
-          errorMessage: 'Username and email need to be unique. Either username or email is already used.'
+        res.status(500).render("auth/signup", {
+          errorMessage: "Username and email need to be unique. Either username or email is already used."
         });
       } else {
         next(error);
@@ -74,45 +76,45 @@ router.post('/signup', shouldNotBeLoggedIn, (req, res, next) => {
 ////////////////////////////////////////////////////////////////////////
 
 // .get() route ==> to display the login form to users
-router.get('/login', shouldNotBeLoggedIn, (req, res) => res.render('auth/login'));
+router.get("/login", isLoggedOut, (req, res) => res.render("auth/login"));
 
 // .post() login route ==> to process form data
-router.post('/login', shouldNotBeLoggedIn, (req, res, next) => {
+router.post("/login", isLoggedOut, (req, res, next) => {
   const { email, password } = req.body;
 
-  if (email === '' || password === '') {
-    res.render('auth/login', {
-      errorMessage: 'Please enter both, email and password to login.'
+  if (email === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both, email and password to login."
     });
     return;
   }
 
   User.findOne({ email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        res.render('auth/login', { errorMessage: 'Email is not registered. Try with other email.' });
+        res.render("auth/login", { errorMessage: "Email is not registered. Try with other email." });
         return;
       } else if (bcryptjs.compareSync(password, user.passwordHash)) {
         req.session.user = user;
-        res.redirect('/user-profile');
+        res.redirect("/user-profile");
       } else {
-        res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        res.render("auth/login", { errorMessage: "Incorrect password." });
       }
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
 ////////////////////////////////////////////////////////////////////////
 ///////////////////////////// LOGOUT ////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 
-router.post('/logout', isLoggedIn, (req, res) => {
+router.post("/logout", isLoggedIn, (req, res) => {
   req.session.destroy();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/user-profile', isLoggedIn, (req, res) => {
-  res.render('users/user-profile');
+router.get("/user-profile", isLoggedIn, (req, res) => {
+  res.render("users/user-profile");
 });
 
 module.exports = router;
